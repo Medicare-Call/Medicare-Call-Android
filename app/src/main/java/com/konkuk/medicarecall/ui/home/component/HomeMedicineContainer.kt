c4a9f26b808af45a3bc5d654668f4a5875a1d5dd
librawish808 <sophiadiary842@gmail.com>
2025-07-07 17:02:50 +0900

    [Chore]: import 수정

--- app/src/main/java/com/konkuk/medicarecall/ui/home/component/HomeMedicineContainer.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/home/component/HomeMedicineContainer.kt
@@ -2,7 +2,6 @@ package com.konkuk.medicarecall.ui.home.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer

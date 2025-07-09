4502f17ccb37166c540d35c6b2ad4c1303b23ec0
ProtossManse <ingan2473@gmail.com>
2025-07-09 11:27:06 +0900

    Feat: 어르신 등록하기 UI 구현

--- app/src/main/java/com/konkuk/medicarecall/ui/login_senior/screen/LoginSeniorInfoScreen.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/login_senior/screen/LoginSeniorInfoScreen.kt
@@ -1,26 +1,39 @@
 package com.konkuk.medicarecall.ui.login_senior.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.konkuk.medicarecall.R
+import com.konkuk.medicarecall.ui.component.CTAButton
 import com.konkuk.medicarecall.ui.component.DefaultDropdown
 import com.konkuk.medicarecall.ui.component.DefaultTextField
 import com.konkuk.medicarecall.ui.component.GenderToggleButton
 import com.konkuk.medicarecall.ui.login_info.component.TopBar
 import com.konkuk.medicarecall.ui.login_info.uistate.LoginUiState
 import com.konkuk.medicarecall.ui.login_info.viewmodel.LoginViewModel
+import com.konkuk.medicarecall.ui.model.CTAButtonType
 import com.konkuk.medicarecall.ui.model.RelationshipType
 import com.konkuk.medicarecall.ui.model.SeniorLivingType
 import com.konkuk.medicarecall.ui.theme.MediCareCallTheme
@@ -111,6 +124,40 @@ fun LoginSeniorInfoScreen(
                 category = "어르신 거주 방식",
                 scrollState
             )
+
+            Spacer(Modifier.height(20.dp))
+
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(14.dp))
+                    .background(MediCareCallTheme.colors.g50)
+                    .border(
+                        1.2.dp,
+                        color = MediCareCallTheme.colors.main,
+                        shape = RoundedCornerShape(14.dp)
+                    )
+            ) {
+                Row(
+                    Modifier
+                        .padding(vertical = 16.dp)
+                        .align(Alignment.Center)
+                ) {
+                    Icon(
+                        painterResource(R.drawable.ic_plus), contentDescription = "플러스 아이콘",
+                        tint = MediCareCallTheme.colors.main
+                    )
+                    Spacer(Modifier.width(8.dp))
+                    Text(
+                        "어르신 더 추가하기",
+                        color = MediCareCallTheme.colors.main,
+                        style = MediCareCallTheme.typography.B_17
+                    )
+                }
+            }
+
+            Spacer(Modifier.height(30.dp))
+            CTAButton(CTAButtonType.GREEN, "다음", {}, modifier.padding(bottom = 20.dp))
 
 
         }

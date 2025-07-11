bc15e593c121b043d9c08d42b43bc98523a62de0
Chae Minji <162430621+alswlekk@users.noreply.github.com>
2025-07-11 17:15:39 +0900

    Fix : 개인정보 설정 화면 TextField 수정

--- app/src/main/java/com/konkuk/medicarecall/ui/settings/screen/PersonalDetailScreen.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/settings/screen/PersonalDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.konkuk.medicarecall.ui.settings.screen
 
+import android.R.attr.phoneNumber
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -23,11 +24,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.konkuk.medicarecall.R
 import com.konkuk.medicarecall.ui.component.CTAButton
 import com.konkuk.medicarecall.ui.component.DefaultDropdown
+import com.konkuk.medicarecall.ui.component.DefaultTextField
 import com.konkuk.medicarecall.ui.component.GenderToggleButton
 import com.konkuk.medicarecall.ui.model.CTAButtonType
 import com.konkuk.medicarecall.ui.model.RelationshipType
@@ -35,11 +38,16 @@ import com.konkuk.medicarecall.ui.model.SeniorLivingType
 import com.konkuk.medicarecall.ui.settings.component.SettingTextField
 import com.konkuk.medicarecall.ui.settings.component.SettingsTopAppBar
 import com.konkuk.medicarecall.ui.theme.MediCareCallTheme
+import com.konkuk.medicarecall.ui.util.DateOfBirthVisualTransformation
+import com.konkuk.medicarecall.ui.util.PhoneNumberVisualTransformation
 
 @Composable
 fun PersonalDetailScreen(modifier: Modifier = Modifier) {
     var isMale by remember { mutableStateOf<Boolean?>(false) }
     val scrollState = rememberScrollState()
+    var name by remember { mutableStateOf("김옥자") }
+    var birth by remember { mutableStateOf("19390928") }
+    var phoneNum by remember { mutableStateOf("01012345678") }
     Column(modifier = Modifier
         .fillMaxSize()
         .background(MediCareCallTheme.colors.bg)) {
@@ -70,8 +78,24 @@ fun PersonalDetailScreen(modifier: Modifier = Modifier) {
             Column(
                 verticalArrangement = Arrangement.spacedBy(20.dp)
             ) {
-                SettingTextField("성함", "김옥자", "성함")
-                SettingTextField("생년월일","1939 / 09 / 18", "YYYY / MM / DD")
+                Column {
+                    DefaultTextField(
+                        value = name,
+                        onValueChange = { name = it },
+                        category = "이름",
+                        placeHolder = "이름"
+                    )
+                }
+                Column {
+                    DefaultTextField(
+                        value = birth,
+                        onValueChange = { birth = it },
+                        category = "생년월일",
+                        placeHolder = "YYYY / MM / DD",
+                        keyboardType = KeyboardType.Number,
+                        visualTransformation = DateOfBirthVisualTransformation()
+                    )
+                }
                 Column() {
                     Text("성별", style = MediCareCallTheme.typography.M_17, color = MediCareCallTheme.colors.gray7)
                     Spacer(modifier = modifier.height(10.dp))
@@ -84,21 +108,31 @@ fun PersonalDetailScreen(modifier: Modifier = Modifier) {
                             }
                     )
                 }
-                SettingTextField("휴대폰 번호","010-1111-1111","010-1234-5678")
+                Column {
+                DefaultTextField(
+                    value = phoneNum,
+                    onValueChange = { phoneNum = it },
+                    placeHolder = "휴대폰 번호",
+                    keyboardType = KeyboardType.Number,
+                    visualTransformation = PhoneNumberVisualTransformation()
+                )}
+                Column() {
                 DefaultDropdown(
                     enumList = RelationshipType.values().map { it.displayName }
                         .toList(),
                     placeHolder = "관계 선택하기",
                     category = "어르신과의 관계",
                     scrollState
-                )
-                DefaultDropdown(
-                    enumList = SeniorLivingType.values().map { it.displayName }
-                        .toList(),
-                    placeHolder = "거주방식을 선택해주세요",
-                    category = "어르신 거주 방식",
-                    scrollState
-                )
+                )}
+                Column {
+                    DefaultDropdown(
+                        enumList = SeniorLivingType.values().map { it.displayName }
+                            .toList(),
+                        placeHolder = "거주방식을 선택해주세요",
+                        category = "어르신 거주 방식",
+                        scrollState
+                    )
+                }
 
 //                Button(
 //                    modifier = modifier.fillMaxWidth().height(50.dp),
@@ -114,7 +148,11 @@ fun PersonalDetailScreen(modifier: Modifier = Modifier) {
 //                }
 
                 CTAButton(
-                    type = CTAButtonType.GREEN,
+                    type = if (name.isNotEmpty() && birth.isNotEmpty() && phoneNum.isNotEmpty()) {
+                        CTAButtonType.GREEN
+                    } else {
+                        CTAButtonType.DISABLED
+                    },
                     text = "확인",
                     onClick = {},
                     modifier = modifier.height(50.dp),

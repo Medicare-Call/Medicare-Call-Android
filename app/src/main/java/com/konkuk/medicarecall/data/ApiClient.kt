8d7a5ed3d07f911306e666f05de519a234fd1777
ProtossManse <ingan2473@gmail.com>
2025-07-23 00:43:54 +0900

    Feat: Client 추가

---
+++ app/src/main/java/com/konkuk/medicarecall/data/ApiClient.kt
@@ -0,0 +1,22 @@
+package com.konkuk.medicarecall.data
+
+import com.konkuk.medicarecall.BuildConfig
+import com.konkuk.medicarecall.data.api.VerificationService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+
+object ApiClient {
+    private val client = OkHttpClient.Builder().build()
+
+    private val json = Json { ignoreUnknownKeys = true }
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(client)
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .build()
+    val verificationService: VerificationService = retrofit.create(VerificationService::class.java)
+}

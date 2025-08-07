6015a6996e0f053582b8c39e559eec8696e96fcd
librawish808 <sophiadiary842@gmail.com>
2025-08-07 17:40:05 +0900

    [Refactor]: 혈당 API 구조 개선

--- app/src/main/java/com/konkuk/medicarecall/ui/homedetail/glucoselevel/data/GlucoseRepository.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/homedetail/glucoselevel/data/GlucoseRepository.kt
@@ -1,24 +1,12 @@
 package com.konkuk.medicarecall.ui.homedetail.glucoselevel.data
 
 import com.konkuk.medicarecall.ui.homedetail.glucoselevel.model.GlucoseGraphResponseDto
-import com.konkuk.medicarecall.ui.homedetail.glucoselevel.model.GlucoseResponseDto
 import com.konkuk.medicarecall.ui.homedetail.glucoselevel.model.GlucoseType
 import javax.inject.Inject
 
 class GlucoseRepository @Inject constructor(
     private val glucoseApi: GlucoseApi
 ) {
-    suspend fun getWeeklyGlucose(
-        guardianId: Int,
-        cursorDate: String? = null,
-        size: Int = 7
-    ): GlucoseResponseDto {
-        return glucoseApi.getWeeklyGlucose(
-            guardianId = guardianId,
-            cursorDate = cursorDate,
-            size = size
-        )
-    }
 
 
     suspend fun getGlucoseGraph(

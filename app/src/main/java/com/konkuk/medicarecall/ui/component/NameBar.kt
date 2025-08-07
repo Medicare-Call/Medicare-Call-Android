5b6c309bc608eaf151dc0764653646d31740da74
librawish808 <sophiadiary842@gmail.com>
2025-08-07 13:52:41 +0900

    [Refactor]: 컴포넌트 패키지로 이동

--- app/src/main/java/com/konkuk/medicarecall/ui/component/NameBar.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/component/NameBar.kt
@@ -1,27 +1,19 @@
-package com.konkuk.medicarecall.ui
+package com.konkuk.medicarecall.ui.component
 
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color

bb2ef5c55d73cc97340cbcae2181f2db0af14878
librawish808 <sophiadiary842@gmail.com>
2025-08-15 01:50:37 +0900

    [Chore]: 건강, 심리 상세화면 카드 최소 높이 지정

--- app/src/main/java/com/konkuk/medicarecall/ui/homedetail/statemental/component/StateMentalDetailCard.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/homedetail/statemental/component/StateMentalDetailCard.kt
@@ -4,9 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -18,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.konkuk.medicarecall.ui.homedetail.statemental.model.MentalUiState
@@ -37,7 +36,7 @@ fun StateMentalDetailCard(
 
     Card(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxWidth().heightIn(min = 160.dp)
             .figmaShadow(
                 group = LocalMediCareCallShadowProvider.current.shadow03,
                 cornerRadius = 14.dp

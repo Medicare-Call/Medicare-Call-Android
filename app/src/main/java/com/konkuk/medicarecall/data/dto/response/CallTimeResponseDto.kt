e03fd036791dcab7cb7345b5b46f115447ac8a59
ProtossManse <ingan2473@gmail.com>
2025-08-16 05:43:02 +0900

    Feat: 시간 등록 여부 확인 플로우 추가

--- app/src/main/java/com/konkuk/medicarecall/data/dto/response/CallTimeResponseDto.kt
+++ app/src/main/java/com/konkuk/medicarecall/data/dto/response/CallTimeResponseDto.kt
@@ -1,5 +1,8 @@
 package com.konkuk.medicarecall.data.dto.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CallTimeResponseDto (
     val firstCallTime: String,
     val secondCallTime: String,

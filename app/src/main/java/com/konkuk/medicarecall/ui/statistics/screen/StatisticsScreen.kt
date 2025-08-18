6dbc0f8b632c2c80d2f2e8c0e14e75c7b61755f7
ProtossManse <ingan2473@gmail.com>
2025-08-18 21:57:40 +0900

    Feat: 주간 통계 로딩 인디케이터 추가

--- app/src/main/java/com/konkuk/medicarecall/ui/statistics/screen/StatisticsScreen.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/statistics/screen/StatisticsScreen.kt
@@ -2,6 +2,7 @@ package com.konkuk.medicarecall.ui.statistics.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -13,12 +14,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -122,7 +125,15 @@ fun StatisticsScreenLayout(
 
         Spacer(modifier = Modifier.height(17.dp))
         when {
-            uiState.isLoading -> { /* ... */
+            uiState.isLoading -> {
+                Box(
+                    Modifier.fillMaxSize()
+                ) {
+                    CircularProgressIndicator(
+                        Modifier.align(Alignment.Center),
+                        color = MediCareCallTheme.colors.main
+                    )
+                }
             }
 
             uiState.error != null -> { /* ... */

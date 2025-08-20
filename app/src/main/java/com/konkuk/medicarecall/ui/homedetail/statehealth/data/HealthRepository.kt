97f87b54303ecf4ff11289ca982d5683cba1f536
ProtossManse <ingan2473@gmail.com>
2025-08-21 06:58:51 +0900

    Fix: 건강징후 및 증상 분석 UI 관련 수정

--- app/src/main/java/com/konkuk/medicarecall/ui/homedetail/statehealth/data/HealthRepository.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/homedetail/statehealth/data/HealthRepository.kt
@@ -4,5 +4,5 @@ import com.konkuk.medicarecall.ui.homedetail.statehealth.model.HealthUiState
 import java.time.LocalDate
 
 interface HealthRepository {
-    suspend fun getHealthUiState(elderId: Int, date: LocalDate): HealthUiState
+    suspend fun getHealthUiState(elderId: Int, date: LocalDate): Result<HealthUiState>
 }
--- app/src/main/java/com/konkuk/medicarecall/ui/homedetail/statehealth/data/HealthRepositoryImpl.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/homedetail/statehealth/data/HealthRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.konkuk.medicarecall.ui.homedetail.statehealth.data
 
+import android.util.Log
 import com.konkuk.medicarecall.ui.homedetail.statehealth.model.HealthUiState
 import java.time.LocalDate
 import javax.inject.Inject
@@ -7,16 +8,16 @@ import javax.inject.Inject
 class HealthRepositoryImpl @Inject constructor(
     private val healthApi: HealthApi
 ) : HealthRepository {
-    override suspend fun getHealthUiState(elderId: Int, date: LocalDate): HealthUiState {
-        return try {
+    override suspend fun getHealthUiState(elderId: Int, date: LocalDate): Result<HealthUiState> =
+        runCatching {
             val response = healthApi.getDailyHealth(elderId, date.toString())
             HealthUiState(
                 symptoms = response.symptomList.orEmpty(),
                 symptomAnalysis = response.analysisComment.orEmpty(),
                 isRecorded = response.symptomList!!.isNotEmpty() || !response.analysisComment.isNullOrBlank()
             )
-        } catch (e: Exception) {
             HealthUiState.EMPTY
         }
-    }
+
+
 }

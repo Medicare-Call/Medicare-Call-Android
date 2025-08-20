9662ebd7b5ecc978a7e5593ff65c60561c686eec
ProtossManse <ingan2473@gmail.com>
2025-08-20 22:22:27 +0900

    Fix: 임포트 수정

--- app/src/main/java/com/konkuk/medicarecall/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/home/HomeViewModel.kt
@@ -1,6 +1,9 @@
 package com.konkuk.medicarecall.ui.home
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope

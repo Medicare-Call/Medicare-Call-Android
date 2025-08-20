2493ad54e5c67d224120dd16cdadb26a818e75e3
ProtossManse <ingan2473@gmail.com>
2025-08-21 02:54:58 +0900

    Fix: 주간통계 식사 UI 맞춤 수정

--- app/src/main/java/com/konkuk/medicarecall/ui/statistics/weeklycard/WeeklyMealCard.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/statistics/weeklycard/WeeklyMealCard.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -36,7 +38,6 @@ fun WeeklyMealCard(
 
     Card(
         modifier = modifier
-            .wrapContentHeight()
             .figmaShadow(
                 group = LocalMediCareCallShadowProvider.current.shadow03,
                 cornerRadius = 14.dp
@@ -66,7 +67,8 @@ fun WeeklyMealCard(
 
             //2) 아침+점심+저녁
             Column(
-                verticalArrangement = Arrangement.spacedBy(0.dp)
+                Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceAround
             ) {
                 meal.forEach { weeklyMeal ->
 

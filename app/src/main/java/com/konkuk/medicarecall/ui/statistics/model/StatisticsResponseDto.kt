a753409562f55495b6e27138bc7d1ca5728ba10f
ProtossManse <ingan2473@gmail.com>
2025-08-20 23:10:28 +0900

    Fix: 주간통계 제대로 뜨지 않는 문제 수정

--- app/src/main/java/com/konkuk/medicarecall/ui/statistics/model/StatisticsResponseDto.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/statistics/model/StatisticsResponseDto.kt
@@ -38,11 +38,11 @@ data class SummaryStatsDto(
 @Serializable
 data class MealStatsDto(
     @SerialName("breakfast")
-    val breakfast: Int,
+    val breakfast: Int?,
     @SerialName("lunch")
-    val lunch: Int,
+    val lunch: Int?,
     @SerialName("dinner")
-    val dinner: Int
+    val dinner: Int?
 )
 
 @Serializable
